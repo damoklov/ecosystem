b9311e87c85140f8107e0a90d8f05556b8d79fbc
Mykhailo Pazyniuk <mishanya@protonmail.com>
2019-04-03 22:58:06 +0300

    Added inital v2.0

---
+++ v2.0/river.py
@@ -0,0 +1,80 @@
+import random as rnd
+from animals import Water
+
+
+class River:
+    def __init__(self, animals, length):
+        """Constructor for class River"""
+        self.animals = animals  # list
+        self.ecosystem = [Water('wave', rnd.random()) for i in range(length)]
+
+    def add_animal(self, animal_obj, place):
+        """Adds animal to a list"""
+        try:
+            assert 0 <= place <= len(self.ecosystem) - 1
+            assert isinstance(self.ecosystem[place], Water)
+        except AssertionError:
+            print('Ecosystem has fixed size. Try another place!')
+            return None
+        else:
+            self.ecosystem[place] = animal_obj
+
+    def count_animals(self, name):
+        """Counts animals in river"""
+        counter = 0
+        for place in self.ecosystem:
+            try:
+                if place.name == name:
+                    counter += 1
+                else:
+                    continue
+            except AttributeError:
+                continue
+        return counter
+
+    def generate_ecosystem(self):
+        """Randomly generates river"""
+        for place in range(len(self.animals)):
+            animal = self.animals[place]
+            self.add_animal(animal, place)
+        rnd.shuffle(self.ecosystem)
+        return self.ecosystem
+
+    def update_ecosystem(self, steps=1):
+        """Makes certain amount of steps"""
+        for i in range(steps):
+            for animal in self.ecosystem:
+                try:
+                    self.ecosystem = animal.move(self.ecosystem)
+                except AttributeError:
+                    self.ecosystem = self.ecosystem
+        return self
+
+    def __str__(self):
+        """Represents river as string"""
+        ecosystem_representation = [str() for i in range(len(self.ecosystem))]
+        for i in range(len(self.ecosystem)):
+            try:
+                ecosystem_representation[i] = '[{0}{1:.2f}{2}]'.format(
+                    'F' if self.ecosystem[i].name == 'fish' else 'B',
+                    self.ecosystem[i].strength,
+                    'M' if self.ecosystem[i].gender is True else 'F')
+            except AttributeError:
+                ecosystem_representation[i] = '[~~~~~~]'
+        return ''.join(ecosystem_representation)
+
+    def __repr__(self):
+        """Represents river as string"""
+        ecosystem_representation = [str() for j in range(len(self.ecosystem))]
+        for i in range(len(self.ecosystem)):
+            try:
+                animal = self.ecosystem[i]
+                if animal.name == 'fish':
+                    ecosystem_representation[i] = '\U0001F41F'
+                elif animal.name == 'bear':
+                    ecosystem_representation[i] = '\U0001F43B'
+                ecosystem_representation[i] += '%.2f' % animal.strength\
+                                               + str(animal.gender)[0]
+            except AttributeError:
+                ecosystem_representation[i] = '\U0001F30A'
+        return ''.join(ecosystem_representation)
---
+++ v2.0/simulation.py
@@ -0,0 +1,132 @@
+import time
+import re
+import random as rnd
+from river import River
+from animals import Bear, Fish
+
+
+class Visualization:
+    def __init__(self, river, cycles, bears, fish):
+        """Constructor for class Visualization"""
+        self.cycles = eval(cycles)
+        self.river = eval(river)
+        self.bears = eval(bears)
+        self.fish = eval(fish)
+
+    def test_parameters(self):
+        """Asserts parameters input"""
+        assert isinstance(self.cycles, int)
+        assert isinstance(self.river, int)
+        assert isinstance(self.bears, int)
+        assert isinstance(self.fish, int)
+        assert 1 <= self.cycles <= 5
+        assert 5 <= self.river <= 50
+        assert 1 <= self.bears <= (self.river - 1)
+        assert 1 <= self.fish <= (self.river - 1)
+
+    def create_assets(self):
+        """Creates animals and river"""
+        bears = [Bear('bear', rnd.choice([True, False]), rnd.random()) for i in range(self.bears)]
+        fish = [Fish('fish', rnd.choice([True, False]), rnd.random()) for j in range(self.fish)]
+        animals = bears + fish
+        ecosystem = River(animals, self.river)
+        cycles = self.cycles
+        return animals, ecosystem, cycles
+
+
+def find_matches(searched_text):
+    """Function for finding UTF characters"""
+    char = searched_text.group()
+    assert ord(char) > 0xffff
+    encoded = char.encode('utf-16-le')
+    return (chr(int.from_bytes(encoded[:2], 'little')) +
+            chr(int.from_bytes(encoded[2:], 'little')))
+
+
+def return_recompiled(text):
+    """Function for replacing UTF into BMP with surrogates"""
+    compiled = re.compile(r'[\U00010000-\U0010FFFF]')
+    return compiled.sub(find_matches, text)
+
+
+def read_input():
+    """Gets user input"""
+    print('GREETINGS IN ECOSYSTEM 1.0!')
+    river = input('Enter how big the river should be: ')
+    repeats = input('Enter how many times to repeat each cycle: ')
+    bears = input('Enter maximum amount of bears to create: ')
+    fish = input('Enter maximum amount of fish to create: ')
+    return river, repeats, bears, fish
+
+
+def automatic_simulation():
+    """Automatically creates needed values"""
+    river = '20'
+    repeats = '5'
+    bears = '4'
+    fish = '6'
+    return river, repeats, bears, fish
+
+
+def initialize_parameters(automatic=True):
+    """Apply parameters for ecosystem"""
+    while True:
+        try:
+            if automatic:
+                user_input = automatic_simulation()
+            else:
+                user_input = read_input()
+            visualization = Visualization(*user_input)
+            visualization.test_parameters()
+        except AssertionError:
+            print('Your entered something wrong! Check your input')
+        else:
+            break
+    animals, ecosystem, cycles = visualization.create_assets()
+    return animals, ecosystem, cycles
+
+
+def launch_cycle():
+    """Main loop"""
+    animals, ecosystem, cycles = initialize_parameters()
+    ecosystem.generate_ecosystem()
+
+    print('GREETINGS IN ECOSYSTEM 1.0!')
+    print(ecosystem)
+    print('Bears here: ', ecosystem.count_animals('bear'))
+    print('Fish here: ', ecosystem.count_animals('fish'))
+
+    for step in range(cycles):
+        time.sleep(0.5)
+        new_ecosystem = ecosystem.update_ecosystem(steps=1)
+        # using <steps> only for testing purpose
+
+        print(new_ecosystem)
+        print('Bears here: ', new_ecosystem.count_animals('bear'))
+        print('Fish here: ', new_ecosystem.count_animals('fish'))
+
+
+def string_placement():
+    """Generates string"""
+    animals, ecosystem, cycles = initialize_parameters()
+    ecosystem.generate_ecosystem()
+
+    line_to_return = return_recompiled(repr(ecosystem))
+    line_to_return += '\nBears here: ' + str(ecosystem.count_animals('bear'))
+    line_to_return += '\nFish here: ' + str(ecosystem.count_animals('fish'))
+
+    for step in range(cycles):
+        new_ecosystem = ecosystem.update_ecosystem(
+            steps=1)  # using <steps> only for testing purpose
+
+        line_to_return += '\n' + return_recompiled(repr(new_ecosystem))
+        line_to_return += '\nBears here: ' + str(
+            new_ecosystem.count_animals('bear'))
+        line_to_return += '\nFish here: ' + str(
+            new_ecosystem.count_animals('fish'))
+
+    return line_to_return
+
+
+if __name__ == '__main__':
+    launch_cycle()
